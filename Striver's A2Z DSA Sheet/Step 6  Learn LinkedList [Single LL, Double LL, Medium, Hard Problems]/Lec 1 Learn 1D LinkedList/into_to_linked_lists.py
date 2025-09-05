e6bb621574fe2dccc635c7ff511262cf25716c8b
MahirKhan2005 <mahirkhan418992815@gmail.com>
2025-09-05 11:12:24 +0530

    5th September 2025

---
+++ Striver's A2Z DSA Sheet/Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 1 Learn 1D LinkedList/into_to_linked_lists.py
@@ -0,0 +1,36 @@
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def push_back(self,data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+            return
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = newNode
+
+
+        
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data,end=" -> ")
+            temp = temp.next
+        print("None")
+
+ll = LinkedList()
+ll.push_back(1)
+ll.push_back(2)
+ll.push_back(3)
+
+ll.display()
