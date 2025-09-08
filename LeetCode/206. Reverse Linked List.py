0a1cddbb1a97cf91e0f228fd2b09941f73764056
MahirKhan2005 <mahirkhan418992815@gmail.com>
2025-09-08 11:07:41 +0530

    8th September 2025

---
+++ LeetCode/206. Reverse Linked List.py
@@ -0,0 +1,47 @@
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def push_back(self,data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+            return
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = newNode
+            
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data,end=" -> ")
+            temp = temp.next
+        print("None")
+
+    def reverseList(self):
+        front = self.head
+        prev = None
+        while front:
+            temp = front
+            front = front.next
+            temp.next = prev
+            prev = temp
+        self.head = prev
+
+ll = LinkedList()
+ll.push_back(1)
+ll.push_back(2)
+ll.push_back(3)
+ll.push_back(4)
+ll.push_back(5)
+
+ll.reverseList()
+ll.display()
