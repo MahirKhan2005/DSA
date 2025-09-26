2488c8337d04f5babb74b25672cf8cfd1b2629ce
MahirKhan2005 <mahirkhan418992815@gmail.com>
2025-09-26 08:01:35 +0530

    26th September 2025

--- LeetCode/231. Power of Two.py
+++ LeetCode/231. Power of Two.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # Loop version - TC - O(logn), SC - O(1)
     def isPowerOfTwo(self, n):
         while n>0:
             if n==1:
@@ -7,5 +8,10 @@ class Solution(object):
                 return False
             n//=2
         return False
+
+    # Bit manipulation 1 liner - TC,SC - O(1)
+    def isPowerOfTwo(self, n: int) -> bool:
+        return True if n>0 and (n & (n-1)==0) else False
+        
 a = Solution()
 print(a.isPowerOfTwo(3))
